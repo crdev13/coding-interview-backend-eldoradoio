2ac8b3af3535dd4c48b2ea87ece26a52c4da4331
Carlos Coronado <coronadorodrigo31@gmail.com>
2025-07-20 15:56:25 -0400

    feat: add GET /items/{id} route and handler

--- src/item.handlers.ts
+++ src/item.handlers.ts
@@ -1,5 +1,12 @@
+import { Request, ResponseToolkit } from '@hapi/hapi';
 import * as ItemService from './item.service';
 
 export const getItems = async () => {
     return await ItemService.getItems();
+};
+
+export const getItemById = async (request: Request, h: ResponseToolkit) => {
+    const id = parseInt(request.params.id);
+    const item = await ItemService.getItemById(id);
+    return item ?? h.response({ error: 'Item not found' }).code(404);
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Server } from '@hapi/hapi';
 import * as Handlers from './item.handlers';
+import Joi from 'joi';
 
 export const defineRoutes = (server: Server) => {
     server.route({
@@ -16,5 +17,24 @@ export const defineRoutes = (server: Server) => {
         method: 'GET',
         path: '/items',
         handler: Handlers.getItems,
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/items/{id}',
+        options: {
+            validate: {
+                params: Joi.object({
+                    id: Joi.number().integer().positive().required(),
+                }),
+                failAction: (request, h, err: any) => {
+                    return h
+                        .response({ error: err.message })
+                        .code(400)
+                        .takeover();
+                },
+            },
+        },
+        handler: Handlers.getItemById,
     });
 };
